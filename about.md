5da255db187e9fefe07cd36cf471bca76ff1a3ac
xiayue-fe <xiayue_fe@hotmail.com>
2016-02-23 20:31:25 +0800

    1

--- about.md
+++ about.md
@@ -11,7 +11,7 @@ header-img: "img/green.jpg"
 Hi,我是xiayue，欢迎光临我的博客！
 
 
-###自我定位
+### 自我定位
 
 
 - 我原本是一名java程序员
@@ -22,14 +22,29 @@ Hi,我是xiayue，欢迎光临我的博客！
 
 
 
-###自学练习作品
+### 自学练习作品
 
 - [练习列表](http://xiayue-fe.github.io/demos/)
 
 
-###相关技能
+### 相关技能
+
+- 熟练掌握语义化的 HTML 和具有兼容性的 CSS 模式，熟练手写符合 W3C 标准的结构和代码
+- 熟练掌握 jQuery，实现日常需要的交互效果
+- 能够使用git和svn进行版本管理
+- 能够书写基本的js原生代码
+- 对于js模块化有一定的了解，自学了requirejs的基本使用
+- 对 HTML5 和 CSS3 有一定了解
+- 对前端canvas，svg有一定了解
+- 对nodejs和gulp相关内容有一定了解
+- 熟悉java，有相关的后端知识
+- 喜欢接触新事物，有一定的自学能力
 
 
+### 简历
+
+-[在线简历](http://xiayue.deercv.com/?preview=1)
+-[pdf版下载](http://xiayue-fe.github.io/jianli/web前端-黄浩-2年.pdf)
 
 
 
---
+++ practice/validate.js
@@ -0,0 +1,105 @@
+﻿(function () {
+    $validate = window.$validate = function (options) {
+        return new $validate.prototype.init(options);	
+    };
+    $validate.prototype = {
+        valilist: [
+            { name: "isEmail", expression: /^\w+([\.\-]\w+)*\@\w+([\.\-]\w+)*\.\w+$/, message: "邮箱格式不正确" },
+            { name: "isNull", expression: /\S/, message: "此项不能为空" },
+            {name: "isMobile",expression:/^1\d{10}$/,message:"电话号码格式不正确"},
+            {name: "isID",expression:/^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{4}$/,message:"请填写18位身份证号"}
+        //自己加
+        ],
+        extend: function EvUpdate(NewObject, OldObject) {
+            function clonePrototype() { }
+            clonePrototype.prototype = NewObject;
+            var obj = new clonePrototype();
+            for (var ele in obj) {
+                if (typeof (obj[ele]) == "object")
+                    EvUpdate(obj[ele], OldObject[ele]);
+                else
+                    OldObject[ele] = obj[ele];
+            }
+        },
+        config: { doc: null, realtime: false, callback: null, uiback: null, tags: "input", valiattr: "data-validate-option", valimessageattr: "data-validate-message" },
+        init: function (options) {
+            this.extend(options, this.config);
+            if (typeof (this.config.doc) == "string") {
+                this.config.doc = document.getElementById(this.config.doc);
+            }
+            return this;
+        },
+        serch: function (name) {
+            for (var i = 0; i < this.valilist.length; i++) {
+                if (name == this.valilist[i].name) {
+                    return this.valilist[i];
+                }
+            }
+            return null;
+        },
+        begin: function () {
+            if (!this.config.doc) {
+                return false;
+            }
+            if (this.config.doc.tagName.toLowerCase() != "input" && this.config.doc.tagName.toLowerCase() != "select") {
+                var alls = new Array();
+                alls.push(this.config.doc.getElementsByTagName("input"));
+                alls.push(this.config.doc.getElementsByTagName("select"));
+                for (var i = 0; i < alls.length; i++) {
+                    for (var y = 0; y < alls[i].length; y++) {
+                        var valitype = alls[i][y].getAttribute(this.config.valiattr);
+                        var valione = this.serch(valitype);
+                        if (valitype && valione) {
+                            var value = alls[i][y].value;
+                            var message = alls[i][y].getAttribute(this.config.valimessageattr);
+                            var result = value.match(valione.expression);
+                            if (result == null) {
+                                this.show(alls[i][y], valione, message);
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                var valitype = this.config.doc.getAttribute(this.config.valiattr);
+                var valione = this.serch(valitype);
+                if (valitype && valione) {
+                    var value = this.config.doc.value;
+                    var message = this.config.doc.getAttribute(this.config.valimessageattr);
+                    var result = value.match(valione.expression);
+                    if (result == null) {
+                        this.show(this.config.doc, valione, message);
+                        return false;
+                    }
+                }
+            }
+            return true;
+        },
+        show: function (domone, valione, message) {
+            if (this.config.uiback == null) {
+                domone.focus();
+                var valimessage = message != "" ? message : valione.message;
+                var _hg = domone.clientHeight;
+                var t = domone.offsetTop;
+                var l = domone.offsetLeft;
+                while (domone = domone.offsetParent) {
+                    t += domone.offsetTop;
+                    l += domone.offsetLeft;
+                };
+                t = t + _hg;
+                function parseNode(arg) { var objE = document.createElement("div"); objE.innerHTML = arg; return objE.childNodes[0]; };
+                var str = "<div id=\"_lxpfloattip_\" onclick=\"hidefloattooltip()\" style=\"width:" + _hg + "px;z-index: 800000; position: absolute; left: " + l + "px; top: " + t + "px;\"><table cellspacing=\"0\" cellpadding=\"0\" border=\"0\" style=\"width:200px\"><tbody><tr style=\"height:auto\"><td class=\"tip_lefttop\"></td><td class=\"tip_top\"></td><td class=\"tip_righttop\"></td></tr><tr><td class=\"tip_left\"></td><td id=\"_lxpfloattip_value\" bgcolor=\"#ffffda\" style=\"color:navy;font-size:14px\">" + valimessage + "</td><td class=\"tip_right\"></td></tr><tr><td class=\"tip_leftbottom\"></td><td class=\"tip_bottom\"></td><td class=\"tip_rightbottom\"></td></tr></tbody></table></div>";
+                var validatedemo = parseNode(str);
+                document.body.appendChild(validatedemo);
+                setTimeout("document.body.removeChild(document.getElementById(\"_lxpfloattip_\"))", 2000);
+            }
+            else {
+                domone.focus();
+                this.config.uiback(domone, valione, message);
+                //调用客户自定义的提示UI进行提示
+            }
+        }
+    };
+    $validate.prototype.init.prototype = $validate.prototype;
+})();
